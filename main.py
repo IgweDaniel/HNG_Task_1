72f9567a66bacb55f0c4c24bf387464d87e92c29
IgweDaniel <danielchibuzoigwe@gmail.com>
2023-09-07 14:32:59 +0100

    adding github paths

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ def read_root(slack_name: str, track:str):
         "current_day":dt.strftime('%A'),
         "utc_time":dt.timestamp() ,
         "track":track,
-        "github_file_url":"",
-        "github_repo_url":"",
-        "status_code":""
+        "github_file_url":"https://github.com/IgweDaniel/HNG_Task_1/blob/main/main.py",
+        "github_repo_url":"https://github.com/IgweDaniel/HNG_Task_1",
+        "status_code":200
     }
 
 
