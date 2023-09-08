c7aff909935c2e1bdb44840eaf3667c5f59c2ba8
IgweDaniel <danielchibuzoigwe@gmail.com>
2023-09-08 11:42:26 +0100

    fixed utc time format

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ app = FastAPI()
 
 @app.get("/api")
 def read_root(slack_name: str, track:str):
-    dt = datetime.now(timezone.utc)
+    current_time_utc = datetime.utcnow()
+
+# Format it as a string in the desired format
+    formatted_time = current_time_utc.strftime('%Y-%m-%dT%H:%M:%SZ')
     return {
         "slack_name":slack_name,
-        "current_day":dt.strftime('%A'),
-        "utc_time":dt.timestamp() ,
+        "current_day":current_time_utc.strftime('%A'),
+        "utc_time":formatted_time ,
         "track":track,
         "github_file_url":"https://github.com/IgweDaniel/HNG_Task_1/blob/main/main.py",
         "github_repo_url":"https://github.com/IgweDaniel/HNG_Task_1",
